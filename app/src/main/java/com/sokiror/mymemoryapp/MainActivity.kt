c90b4d2c38de87661b6c189ee6a80345ce846cd1
sokiror <okiror1vinald@gmail.com>
2025-04-12 12:30:48 +0300

    adapter

--- app/src/main/java/com/sokiror/mymemoryapp/MainActivity.kt
+++ app/src/main/java/com/sokiror/mymemoryapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sokiror.mymemoryapp.ui.theme.MyMemoryAppTheme
 
@@ -27,5 +28,12 @@ class MainActivity : ComponentActivity() {
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
+        
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
+        
     }
+
+    private fun MemoryBoardAdapter(activity: MainActivity, i: Int) {}
 }
